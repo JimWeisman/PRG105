81590033068b4af7c2250b0ccce56b7fbbebe509
Jim Weisman <jweisman@students.mchenry.edu>
2019-01-30 12:06:42 -0600

    Add files via upload

---
+++ printing.py
@@ -0,0 +1,33 @@
+# Printing the song Over The River And Through The Woods
+# Performed by Rachel Rambach
+
+print("Over the river and through the woods,")
+print("To grandmother's house we go;")
+print("The horse knows the way to carry the sleigh,")
+print("Through (the) white and drifted snow!\n")
+
+print("Over the river and through the woods,")
+print("Oh, how the wind does blow!")
+print("It stings the toes and bites the nose,")
+print("As over the ground we go.\n")
+
+print("Over the river and through the woods,")
+print("To have a first-rate play;")
+print("Oh, hear the bells ring, \"Ting-a-ling-ling\"!")
+print("Hurrah for Thanksgiving Day! \n")
+
+print("Over the river and through the woods,")
+print("Trot fast, my dapple gray!")
+print("Spring over the ground,")
+print("Like a hunting hound!")
+print("For this is Thanksgiving Day. \n")
+
+print("Over the river and through the woods,")
+print("And straight through the barnyard gate.")
+print("We seem to go extremely slow ")
+print("It is so hard to wait! \n")
+
+print("Over the river and through the woods,")
+print("Now Grandmother's cap I spy!")
+print("Hurrah for the fun! Is the pudding done?")
+print(" Hurrah for the pumpkin pie!\n")
