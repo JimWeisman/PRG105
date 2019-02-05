fe7b099117266d0863387df8f85c00d5bbe98e1b
Jim Weisman <jweisman@students.mchenry.edu>
2019-02-05 11:59:35 -0600

    Add files via upload

---
+++ weisman_madlib_song.py
@@ -0,0 +1,64 @@
+# 2.2 Medlibs coded by Jim Weisman
+
+
+# variables
+place1 = input("enter something you find in your yard")
+place2 = input("enter something else you find in your yard")
+place3 = input("enter a 3th thing you find in your yard")
+place4 = input("enter a 4th thing you find in your yard")
+yourname = input("enter your name")
+animal = input("enter a type of animal")
+job = input("enter your job")
+verb = input("enter a verb")
+object = input("enter an object")
+holiday1 = input("enter a holiday")
+holiday2 = input("enter another holiday")
+petname1 = input("what is your pets name")
+petname2 = input("do you have another pet? and what is its name")
+petname3 = input("do you have a 3rd pet? and what is its name")
+friend1 = input("enter a friends name")
+friend2 = input("enter another friends name")
+color = input("what is your favoret color?")
+food = input("what is your favorite food?")
+
+print("=" * 10, "The Song", "=" * 10)
+
+
+print("Over the " + place1 + " and through the " + place2 + " ,")
+print("To " + yourname +  " house we go;")
+print("The " + animal + " knows the way to carry the sleigh,")
+print("Through (the) " + color +  " and drifted snow!\n")
+
+print("Over the " + place2 + " and through the " + place3 + ",")
+print("Oh, how the:" + job + " does blow!")
+print("It stings the toes and bites the nose,")
+print("As over the ground we go.\n")
+
+print("Over the " + place3 + " and through the " + place4 + ",")
+print("To have a " + verb + " play;")
+print("Oh, hear the " + object + " ring, \"Ting-a-ling-ling\"!")
+print("Hurrah for " + holiday1 + " Day! \n")
+
+print("Over the " + place1 + " and through the " + place4 + ",")
+print("Trot fast, my " + petname1 +  " gray!")
+print("Spring over the ground,")
+print("Like an" + petname2 + " hound!")
+print("For this is " + holiday2 + " Day. \n")
+
+print("Over the " + place2 + " and through the " + place4 + ",")
+print("And straight through the " + color + " gate.")
+print("We seem to go extremely slow ")
+print("It is so hard to wait! \n")
+
+print("Over the " + place1 + " and through the " + place3 + ",")
+print("Now " + friend1 + "'s cap I spy!")
+print("Hurrah for the fun! Is the pudding done?")
+print("Hurrah for the " + food + " pie!\n")
+
+
+
+
+
+
+
+
