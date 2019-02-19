861b84747d89af7ea802192cf938a8483f903c0f
Jim Weisman <jweisman@students.mchenry.edu>
2019-02-19 11:23:32 -0600

    Add files via upload

---
+++ 3.1_Simple If Statements Days of the week.py
@@ -0,0 +1,22 @@
+#jim weisman
+#spring2019
+#PRG105
+
+day = int(input("Choose a number between 1 and 7: "))
+
+if day == 1:
+    print("today is Monday")
+elif day == 2:
+    print("today is Tuesday")
+elif day == 3:
+    print("today is Wednesday")
+elif day == 4:
+    print("today is Thursday")
+elif day == 5:
+    print("today is Friday")
+elif day == 6:
+    print("today is Saturday")
+elif day == 7:
+    print("today is Sunday")
+else:
+    print("That is not a valid number. Please try again and pick a number between 1 and 7")
