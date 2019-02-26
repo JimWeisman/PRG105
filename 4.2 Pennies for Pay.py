87df2bd4a202fef9da6e7a81f023230aae27a61a
Jim Weisman <jweisman@students.mchenry.edu>
2019-02-26 13:29:41 -0600

    Add files via upload

---
+++ 4.2 Pennies for Pay.py
@@ -0,0 +1,15 @@
+days_worked = int(input("Enter how many days did you work: "))
+money = .01
+pennies = 0
+
+print("Days Worked | Amount Earned That Day")
+print("------------------------------------")
+
+for day in range(days_worked):
+    money = 2 * money
+    pennies += money
+    print(format(day, "20,.0f") + " | $ " + format(money, "20,.2f"))
+
+total = pennies * .01
+
+print("\nTotal Earned over", str(days_worked), "is: $", format(money, ',.2f'))
---
+++ 4.3 Average Rainfall.py
@@ -0,0 +1,16 @@
+print("This program will calculate total rainfall and average monthly rainfall for a period of years.")
+years = int(input("How many years would you like to collect data for? "))
+total_months = 0
+total_rain = 0
+
+for current_year in range(years):
+    print("Year number " + str(current_year + 1))
+    for current_month in ['January', 'February', 'March', "April", 'May', 'June', 'July', 'August',
+                          'September', 'October', 'November', 'December']:
+        monthly_rainfall = float(input("How much rain fell in the month of " + str(current_month) + ": "))
+        total_months += 1
+        total_rain += monthly_rainfall
+
+average_rain = total_rain / total_months
+print("Total amount of rainfall was: " + format(total_rain, ".2f") + " inches.")
+print("Average monthly rainfall was: " + format(average_rain, ".2f") + " inches.")
