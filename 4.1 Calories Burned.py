3a7d39c60788a25a505540772a69fd4fabde9da6
Jim Weisman <jweisman@students.mchenry.edu>
2019-02-26 13:33:00 -0600

    Update 4.1 Calories Burned.py

--- 4.1 Calories Burned.py
+++ 4.1 Calories Burned.py
@@ -1,3 +1,9 @@
+""" 
+jim weisman
+spring 2019
+PGR105
+"""
+
 # Running on a treadmill you burn 4.2 calories per minute.
 # Write a program that uses a loop to display the number of calories burned after 10, 15, 20, 25 and 30 minutes.
 
